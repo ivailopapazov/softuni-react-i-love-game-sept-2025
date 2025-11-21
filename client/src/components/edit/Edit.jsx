6e30af23580ab518e831a5967831473076c26424
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-11-21 20:03:20 +0200

    Show dynamic data on edit page

--- client/src/components/edit/Edit.jsx
+++ client/src/components/edit/Edit.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import request from "../../utils/request";
 
 const initialValues = {
     title: '',
@@ -10,6 +12,7 @@ const initialValues = {
 };
 
 export default function Edit() {
+    const { gameId } = useParams();
     const [values, setValues] = useState(initialValues);
 
     const changeHandler = (e) => {
@@ -18,6 +21,16 @@ export default function Edit() {
             [e.target.name]: e.target.value,
         }));
     };
+
+    useEffect(() => {
+        request(`http://localhost:3030/jsonstore/games/${gameId}`)
+            .then(result => {
+                setValues(result);
+            })
+            .catch(err => {
+                alert(err.message);
+            })
+    }, [gameId]);
 
     return (
         <section id="edit-page">
