f6e5e051636208803103b0c612620d98c92916fc
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-12-04 21:55:00 +0200

    Add user state persistance

--- client/src/contexts/UserContext.jsx
+++ client/src/contexts/UserContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useState } from "react";
 import useRequest from "../hooks/useRequest";
+import usePersistedState from "../hooks/usePersistedState";
 
 const UserContext = createContext({
     isAuthenticated: false,
@@ -18,7 +19,7 @@ const UserContext = createContext({
 export function UserProvider({
     children,
 }) {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = usePersistedState(null);
     const { request } = useRequest();
 
     const registerHandler = async (email, password) => {
